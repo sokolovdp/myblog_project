85552b345031381536bc483a22e348b74dd8b4ce
Dmitrii Sokolov <sokolovdp@gmail.com>
2017-09-10 01:07:03 +0300

    correct confirm-delete form -> method="POST"

--- myblog_project/settings.py
+++ myblog_project/settings.py
@@ -100,7 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'ru-RU'
+LANGUAGE_CODE = 'en-US'
 
 TIME_ZONE = 'UTC'
 
